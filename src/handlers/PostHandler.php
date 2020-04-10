a7a95d1080be69c80e41e47c343057a78086dfbe
donaukinder <higor.carvalho94@gmail.com>
2020-04-10 10:25:51 -0400

    Feed 2/4

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -3,6 +3,7 @@
 namespace src\handlers;
 
 use src\models\Post;
+use src\models\User;
 use src\models\UserRelation;
 
 class PostHandler
@@ -29,6 +30,29 @@ class PostHandler
         }
         $users[] = $id_user;
 
-        print_r($users);
+        $postList = Post::select()
+            ->where('id_user', 'in', $users)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $posts = [];
+        foreach ($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->body = $postItem['body'];
+            $newPost->created_at = $postItem['created_at'];
+
+            $newUser = User::select()
+                ->where('id', $postItem['id_user'])
+                ->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+
+            $posts[] = $newPost;
+        }
+        return $posts;
     }
 }
